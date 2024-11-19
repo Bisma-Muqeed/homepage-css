b1d5c74134503f1e6b9e03d520a30e65ea3fa812
Bisma Muqeed <bismamuqeed@gmail.com>
2024-11-20 02:20:16 +0500

    remove commit code in hero.tsx

--- src/app/Component/Hero.tsx
+++ src/app/Component/Hero.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import style from "./Hero.module.css"
-// import Image from 'next/image'
 const hero = () => {
   return (
     <div>
       <div className = {style.heroContainer}>
-        {/* <Image
-        src ="/image/Hero.svg"
-        alt = "Food Image"
-        layout="fill"
-        objectFit="cover"/> */}
       </div>
-      
     </div>
   )
 }
